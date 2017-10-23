191750477a44e477f372361bee62a546498e0b3a
VIJITH <vijith.v@intl.verizon.com>
2017-10-23 19:04:36 +0530

    added config

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+	port = process.env.PORT || 5000,
 	app = express();
 
 app.use(express.static(__dirname + '/public'));
@@ -7,5 +8,6 @@ app.get('/', function(req, res) {
     res.render('index.html');
 });
 
-app.listen(8080);
-console.log('Listening on port 8080');
+app.listen(port, function() {
+	console.log('Listening on port ' + port);
+});
